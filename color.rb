aae75fdd474f2e9825b7eca38a27bdab63061863
Alyssa Pohahau <alyssapohahau@gmail.com>
2012-08-01 18:10:45 -0700

    able to expand length 3 hex to length 6

--- color.rb
+++ color.rb
@@ -25,7 +25,6 @@ class ColorPalette
         rescue
             abort("Not a valid link. Try another one.")
         end
-        urls << url # will check homepage just in case there is embedded css
         page.css('head').css('link[rel=stylesheet]').each {|stylesheet|
             if !stylesheet['href'].start_with? '/' 
                 css_url = stylesheet['href']
@@ -47,17 +46,20 @@ class ColorPalette
             # group 5: a color (as an English word -- ex. white)
             color_array = page_source.scan(/color\s*:\s*(#[0-9A-Fa-f]{3,6}+)\s*|rgba?\s*\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*|color\s*:\s*(white|aqua|black|blue|fuchsia|gray|green|lime|maroon|navy|olive|orange|purple)/)
             color_array.each{|color|
-        #        puts color.inspect
+                #puts color.inspect
                 if color[0] != nil
                     color = color[0].downcase
+                    color = expand_hex(color) if color.length == 4 # usually length 3 but including '#' so use length 4
                 elsif color[1] != nil
                     color = "#" + color[1].to_i.to_s(16) + color[2].to_i.to_s(16) + color[3].to_i.to_s(16)
+                    # below is the case that you have r: 0 g: 0 b: 0 => #000 .... need to expand
+                    color = expand_hex(color) if color.length == 4 # usually length 3 but including '#' so use length 4 
                 else
                     color = color[4].downcase
                     hex_color = get_hex(color)
-                    color = hex_color if hex_color != nil # try to assign a hex value, if not, will remain as is (ex. white, blue, etc.)
+                    color = hex_color if hex_color != nil # try to assign a hex value, if not, will remain as is.
                 end
-    #            puts color
+                #puts color
                 if @color_map[color] == nil
                     @color_map[color] = 1   
                 else
@@ -83,7 +85,16 @@ class ColorPalette
         when "purple"   then "#800080"
         end  
     end
-
+    
+    # helper method for build_color_palette
+    def expand_hex(color)
+        new_hex = color[0]
+        for i in 1..3
+            new_hex += color[i]+color[i]
+        end
+        new_hex
+    end
+    
     # will sort hash map and add those keys to an array then return array
     def sort_palette
         @color_palette = Array.new
@@ -100,12 +111,12 @@ class ColorPalette
     end
 
     def print_palette_with_freq
-        puts "num colors: " + @color_map.size.to_s
+        puts "number of unique colors: " + @color_map.size.to_s
         printf("--------|-----------\n")
         printf("%.8s\t|%s\n", "Color", "Frequency")
         printf("--------|-----------\n")
         @color_map.each{|key, value|
-            printf("%.8s\t|%d\n", key, value)
+            printf("%.8s |%d\n", key, value)
         }
 
     end
